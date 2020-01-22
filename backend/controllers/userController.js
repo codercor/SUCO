268f49d8f5bc0d4066f120bc10b4c828500eb577
mustafacor <mmustafaa9@gmail.com>
2020-01-22 19:42:18 +0300

    pp upload finish !

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,33 +1,32 @@
 const router = require('express').Router();
 const userModel = require('../models/userModel');
-let jwt = require('jsonwebtoken');
-let auth = require('../authCheck');
-var multer  = require('multer');
-let token = "";
-let ppDiscStorage = multer.diskStorage({
-    // dosya yolunu callback function'ın 2. parametresinde belirtiyoruz.
-    destination: function (req, file, callback) {
-      callback(null, 'public/userpp/');
-      
-    },
-    // dosya ismini ve uzantısını burada oluşturuyoruz.
-    filename: function (req, file, callback) {
-      // HTML input'tan gelen body verisini filename olarak kullanıyoruz.
-      var fileName = file.originalname;
-      
-    //   let token = jwt.verify(req.body.token,require('../config').api_secret_key);
-    //   console.log(token);
-      
-      // mimetype'ın tam listesini araştırıp bulabilirsiniz. 
-      // burada örnek olarak 3 tanesini kullandım.
-      var extension = { 'image/jpeg': '.jpg', 'image/png': '.png', 'image/gif': '.gif' };
-      callback(null, fileName + '-' + Date.now() + extension[file.mimetype]);
-    },
+let jwt = require('jsonwebtoken'),
+    fs = require('fs'),
+    auth = require('../authCheck'),
+    multer = require('multer'),
+    ppDiscStorage = multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, 'public/userpp/');
+        },
+        filename: function (req, file, callback) {
+            var fileName = file.originalname;
+            var extension = { 'image/jpeg': '.jpg', 'image/png': '.png', 'image/gif': '.gif' };
+            callback(null, fileName + '-' + Date.now() + extension[file.mimetype]);
+        },
 
-  });
-var pp = multer({storage:ppDiscStorage}).single('pp');
+    }),
+    fileFilter = function (req, file, cb) {
+        let isImage = (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif')
+        if (isImage) {
+            cb(null, true)
+        } else {
+            cb(null, false)
+            cb(new Error('I don\'t have a clue!'))
+        }
+    },
+    pp = multer({ storage: ppDiscStorage, fileFilter }).single('pp');
 
-    
+
 router.post('/register', (req, res) => {
     console.log("Yeni Kullanıcı Kayıt İsteği");
     userModel.register(req.body).then((status) => {
@@ -41,24 +40,38 @@ router.post('/login', (req, res) => {
     userModel.checkUser(userName, password).then((result) => {
         if (result.length == 1) {
             let token = jwt.sign({ userName, password }, require("../config").api_secret_key, { expiresIn: 500 })
-            res.json({token,login:'successful'});
-        }else{
-            res.json({login:'failed'});
+            res.json({ token, login: 'successful' });
+        } else {
+            res.json({ login: 'failed' });
         }
     });
 });
 
-router.post('/updatePP',(req,res)=>{  
+router.post('/updatePP', (req, res) => {
 
-    
-    pp(req,res,(err)=>{
-        if(err) res.json({error:'file type error'});
-        var imagePath = req.file.path.replace(/^public\/userpp\//, '');
-        console.log(imagePath);
-        
-        res.redirect(imagePath);
+
+    pp(req, res, (err) => {
+
+        if (err) res.json({ error: 'file type error' });
+        else {
+            let token = req.body.token;
+            try {
+                token =  jwt.verify(token, require("../config").api_secret_key);
+                // OOOY OY
+                fs.renameSync(req.file.path,req.file.destination+token.userName+'.'+req.file.originalname.split('.')[1]);
+                
+                console.log(req.file);
+                
+            } catch (error) {
+                fs.unlinkSync(req.file.path);
+                
+                res.send("Giriş Yap");
+                
+            }
+        }
+
     });
-    
+
 });
 
 module.exports = router;
