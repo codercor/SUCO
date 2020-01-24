1ab004f965b29a3f4f75373aadd161d97267f42f
mustafacor <mmustafaa9@gmail.com>
2020-01-24 13:40:20 +0300

    cover photo update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,17 +15,27 @@ let jwt = require('jsonwebtoken'),
         },
 
     }),
+    cpDiscStorage = multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, 'public/usercp/');
+        },
+        filename: function (req, file, callback) {
+            var fileName = file.originalname;
+            var extension = { 'image/jpeg': '.jpg', 'image/png': '.png', 'image/gif': '.gif' };
+            callback(null, fileName + '-' + Date.now() + extension[file.mimetype]);
+        },
+    }),
     fileFilter = function (req, file, cb) {
         let isImage = (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif')
         if (isImage) {
             cb(null, true)
         } else {
             cb(null, false)
-            cb(new Error('I don\'t have a clue!'))
+            cb(new Error('Bu bir resim Değil !!'))
         }
     },
-    pp = multer({ storage: ppDiscStorage, fileFilter }).single('pp');
-
+    pp = multer({ storage: ppDiscStorage, fileFilter: fileFilter }).single('pp'),
+    cp = multer({ storage: cpDiscStorage, fileFilter: fileFilter }).single('pp');
 
 router.post('/register', (req, res) => {
     console.log("Yeni Kullanıcı Kayıt İsteği");
@@ -39,7 +49,7 @@ router.post('/login', (req, res) => {
     let { userName, password } = req.body;
     userModel.checkUser(userName, password).then((result) => {
         if (result.length == 1) {
-            let token = jwt.sign({ userName, password }, require("../config").api_secret_key, { expiresIn: 500 })
+            let token = jwt.sign({ userName, password }, require("../config").api_secret_key, { expiresIn: 5000})
             res.json({ token, login: 'successful' });
         } else {
             res.json({ login: 'failed' });
@@ -47,29 +57,55 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/updatePP', (req, res) => {
-
-
-    pp(req, res, (err) => {
+router.post('/updatePP', async(req, res) => {
+    pp(req, res, async(err) => {
 
         if (err) res.json({ error: 'file type error' });
         else {
             let token = req.body.token;
             try {
-                token =  jwt.verify(token, require("../config").api_secret_key);
+                token = jwt.verify(token, require("../config").api_secret_key);
                 // OOOY OY
-                fs.renameSync(req.file.path,req.file.destination+token.userName+'.'+req.file.originalname.split('.')[1]);
-                
-                console.log(req.file);
-                
+                let userName = token.userName;
+                let userData = await userModel.getIdbyUserName(userName);
+                let photoName = token.userName+userData[0].id + '.' + req.file.originalname.split('.')[1];
+                fs.renameSync(req.file.path, req.file.destination + photoName);
+                await userModel.updatePP(userData[0].id,'/userpp/' + photoName);
+                res.send(`<img src="/userpp/${photoName}" />`);
+                //res.json({ update: "successfull", src:'/userpp/' + photoName})
+
             } catch (error) {
                 fs.unlinkSync(req.file.path);
-                
                 res.send("Giriş Yap");
-                
             }
         }
+    });
 
+});
+
+
+router.post('/updateCP', async(req, res) => {
+    cp(req, res, async(err) => {
+
+        if (err) res.json({ error: 'file type error' });
+        else {
+            let token = req.body.token;
+            try {
+                token = jwt.verify(token, require("../config").api_secret_key);
+                // OOOY OY
+                let userName = token.userName;
+                let userData = await userModel.getIdbyUserName(userName);
+                let photoName = token.userName+userData[0].id + '.' + req.file.originalname.split('.')[1];
+                fs.renameSync(req.file.path, req.file.destination + photoName);
+                await userModel.updateCP(userData[0].id,'/usercp/' + photoName);
+                res.send(`<img src="/usercp/${photoName}" />`);
+                //res.json({ update: "successfull", src:'/userpp/' + photoName})
+
+            } catch (error) {
+                fs.unlinkSync(req.file.path);
+                res.send("Giriş Yap");
+            }
+        }
     });
 
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,11 +4,11 @@ let userModel = {};
 
 userModel.register = async (user) => {
     let check = await userModel.checkMail(user.eposta);
- // Kayıtlı mı diye kontrol et 
+    // Kayıtlı mı diye kontrol et 
     if (check.length > 0) {
         return new Promise((resolve, reject) => {
             // Kayıtlı ise gerekli cevabı ver
-            resolve({ register: false, err:"registered" })
+            resolve({ register: false, err: "registered" })
         });
     }
     // Kayıtlı değil ise kaydet
@@ -33,7 +33,7 @@ userModel.checkMail = (mail) => {
     });
 }
 
-userModel.checkUser = (userName,password)=>{
+userModel.checkUser = (userName, password) => {
     return new Promise((resolve, reject) => {
         let sql = `SELECT * FROM kullanicilar WHERE kullaniciAdi = "${userName}" AND sifre="${password}"`;
         con.query(
@@ -41,6 +41,36 @@ userModel.checkUser = (userName,password)=>{
                 if (err) reject(err)
                 resolve(result);
             });
+    });
+}
+
+userModel.getIdbyUserName = (userName) => {
+    return new Promise((resolve, reject) => {
+        let sql = `SELECT * FROM kullanicilar WHERE kullaniciAdi = "${userName}"`;
+        con.query(
+            sql, (err, result) => {
+                if (err) reject(err)
+                resolve(result);
+            });
+    });
+}
+
+userModel.updatePP = (id,ppPath)=>{
+    return new Promise((resolve,reject)=>{
+        let sql =`UPDATE kullanicilar SET profilResmi = "${ppPath}" WHERE id = ${id}`;
+        con.query(sql,(err,result)=>{
+           if(err) reject(err);
+           resolve(result);
+        })
+    });
+}
+userModel.updateCP = (id,cpPath)=>{
+    return new Promise((resolve,reject)=>{
+        let sql =`UPDATE kullanicilar SET kapakResmi = "${cpPath}" WHERE id = ${id}`;
+        con.query(sql,(err,result)=>{
+           if(err) reject(err);
+           resolve(result);
+        })
     });
 }
 module.exports = userModel;
