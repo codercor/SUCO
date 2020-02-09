651154392a9e71cf62a1e4b53b6c70953b84d483
mustafacor <mmustafaa9@gmail.com>
2020-02-09 13:35:36 +0300

    postModel

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -2,8 +2,13 @@ const con = require("./connection");
 let postModel = {};
 
 postModel.publishPost = function(MyId,text,images,emotion,date,privacy) {
-    //bekle
-   
+    return new Promise((resolve,reject)=>{
+       let sql = "INSERT INTO gonderiler(paylasanId,metin,duygu,resim,begeni,tarih,gizlilik) VALUES(?,?,?,?,?,?,?)";
+       con.query(sql,[MyId,text,emotion,images,0,date,privacy],(err,result)=>{
+           if(err) reject(err);
+           else resolve(result);
+       })
+    });
     
 }
 
